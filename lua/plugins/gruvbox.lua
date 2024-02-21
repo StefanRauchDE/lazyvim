7ce5162c76d4c67b597eade917c22a61c6a0e2e4
Stefan Rauch <mail@stefan-rauch.de>
2024-02-21 20:56:11 +0100

    first lazyvim config changes

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,17 @@
+if true then
+  return {}
+end
+
+return {
+
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
